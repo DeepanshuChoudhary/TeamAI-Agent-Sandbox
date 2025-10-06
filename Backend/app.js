2bf4235f28ec9944ef5a2a2ab4045ae5c81fa9b3
Deepanshu Choudhary <deepengineeryt@gmail.com>
2025-10-06 20:25:01 +0530

    register and login working

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import connectdb from './db/db.js';
+import userRoutes from './routes/user.routes.js';
 
 connectdb();
 
@@ -9,6 +10,8 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/users', userRoutes);
 
 app.get('/', (req,res) => {
     res.send('Hello world')
---
+++ Backend/services/user.service.js
@@ -0,0 +1,19 @@
+import userModel from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
+
+export const createUser = async ({
+    email, password
+}) => {
+
+    if(!email || !password) {
+        throw new Error("Email and Password are required")
+    }
+
+    const user = await userModel.create({
+        email, 
+        password: await bcrypt.hash(password, 10)
+    });
+
+    return user;
+
+}
